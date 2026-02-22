56413d97faad83b6c6fe30d8b304410ef9805517
sushil-890 <kumrsushil890@gmail.com>
2026-02-22 15:42:19 +0530

    Event And Eregistration Funtionality (CRUD) Implemented

--- Backend/Routes/EventRoutes.js
+++ Backend/Routes/EventRoutes.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const { verifyToken, checkRole } = require("../Middleware/AuthMiddleware");
+const {
+  createEvent,
+  getAllEvents,
+  getEventById,
+  updateEvent,
+  deleteEvent,
+} = require("../Controllers/EventController");
 
-router.post("/create");
-router.get("/");
-router.get("/:id");
-router.put("/:id");
-router.delete("/:id");
+router.post("/create", verifyToken, checkRole("admin", "clubauthority"), createEvent);
+
+router.get("/", verifyToken, getAllEvents);
+
+router.get("/:id", verifyToken, getEventById);
+
+router.put("/:id", verifyToken, checkRole("admin", "clubauthority"), updateEvent);
+
+router.delete("/:id", verifyToken, checkRole("admin", "clubauthority"), deleteEvent);
 
 module.exports = router;
